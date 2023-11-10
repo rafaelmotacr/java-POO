9369e51019548cce5b824649a78283ceb34628b6
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-10 10:44:00 -0300

    First commit

---
+++ logicaEmJava/src/EX_Linear3.java
@@ -0,0 +1,13 @@
+
+public class EX_Linear3 {
+
+	public EX_Linear3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
