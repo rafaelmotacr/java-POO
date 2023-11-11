f072ede49e3c0fd0e5b0ebe2bb9f33d620f4fc55
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-11 12:52:38 -0300

    Fourth question completed.

---
+++ logicaEmJava/src/EX_Linear4.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class EX_Linear4 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int empNum;
+		int workedHours;
+		float hourlyRate;
+		float salary;
+		
+		System.out.print("> Enter Your Employe Num: ");
+		empNum = sc.nextInt();
+		System.out.print("> Enter Your Worked Hours: ");
+		workedHours = sc.nextInt();
+		System.out.print("> Enter Your Hourly Rate: ");
+		hourlyRate = sc.nextFloat();
+		
+		salary = workedHours * hourlyRate;
+		System.out.println("NUMBER = " + empNum);
+		System.out.printf("SALARY = U$ %.2f", salary);
+		
+		sc.close();
+	}
+
+}
