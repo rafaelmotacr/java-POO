62a9757e473315d28a27b31b3033bead87ae3c61
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-11 12:28:44 -0300

    Third question completed.

--- logicaEmJava/src/EX_Linear3.java
+++ logicaEmJava/src/EX_Linear3.java
@@ -1,13 +1,20 @@
-
+import java.util.Scanner;
 public class EX_Linear3 {
-
-	public EX_Linear3() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int A, B, C, D;
+		int difference;
+		
+		System.out.print("> Enter four numbers: ");
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		difference = A * B - C * D;
+		System.out.print("> DIFFERENCE: " + difference);
+		sc.close();
 	}
 
 }
