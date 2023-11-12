865e5489b2593f79e81bfb1b3fc6fae9c11d8db9
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-11 22:53:34 -0300

    Lexical Changes.

--- logicaEmJava/src/EX_Condicionais6.java
+++ logicaEmJava/src/EX_Condicionais6.java
@@ -6,14 +6,14 @@ public class EX_Condicionais6 {
 		Scanner sc = new Scanner(System.in);
 		
 		double num = sc.nextDouble();
-		if(num < 0 || num > 100) {
+		if(num < 0.0f || num > 100.0f) {
 			System.out.println("Out of interval");
 		}
-		else if(num <= 25) {
+		else if(num <= 25.0f) {
 			System.out.println("Interval [0,25]");
-		}else if(num <= 50) {
+		}else if(num <= 50.0f) {
 			System.out.println("Interval (25, 50]");
-		}else if(num <= 75) {
+		}else if(num <= 75.0f) {
 			System.out.println("Interval (50,75]");
 		}else {
 			System.out.println("Interval (75,100]");
