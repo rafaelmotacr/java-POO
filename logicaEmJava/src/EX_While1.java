3130735ed6624b9f27d6b2b9f2494a8c2d8c1c31
RafaelMotaCR <rafaelmota361@gmail.com>
2023-11-11 23:40:08 -0300

    First question completed.

---
+++ logicaEmJava/src/EX_While1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EX_While1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int password = sc.nextInt();
+		while(password != 2002){
+			System.out.println("Invalid Password");
+			password = sc.nextInt();
+		}
+		System.out.println("Access allowed");
+		
+		sc.close();
+
+	}
+
+}
